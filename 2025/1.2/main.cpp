11619bee671c29602316dcc7b217c99c396680c9
magarine <magarine@mailbox.org>
2025-12-01 20:03:41 +0100

    day1 part2

--- 2025/1.2/main.cpp
+++ 2025/1.2/main.cpp
@@ -38,20 +38,42 @@ int computePassword(const vector<Rotation>& input) {
     cout << "The dial starts by pointing at " << dial << "." << endl;
 
     for (const auto& r : input) {
-        int delta = (r.dir == 'L') ? -r.value : r.value;
+        int pointAtZero = 0;
 
-        dial = (((dial + delta) % 100) + 100) % 100;
+        if (r.dir == 'L') {
+            // Calculate how many full -100 cycles we complete
+            pointAtZero = abs((dial - r.value) / 100);
 
-        if (dial == 0) {
-            count++;
+            // Check if we cross or land on 0 during the rotation
+            // Don't count if we start at 0 (dial != 0)
+            // Count if the result is 0 or negative (dial - r.value <= 0)
+            if (dial != 0 && dial - r.value <= 0) {
+                pointAtZero++;
+            }
+
+            // Normalize dial position to range 0-99
+            // Double modulo ensures positive result even for negative numbers
+            dial = (((dial - r.value) % 100) + 100) % 100;
+        }
+        else if (r.dir == 'R') {
+            // Calculate how many full 100 cycles we complete
+            pointAtZero = (dial + r.value) / 100;
+
+            // Normalize dial position to range 0-99
+            dial = (dial + r.value) % 100;
         }
 
+        count += pointAtZero;
+
         cout << "The dial is rotated " << r.dir << r.value
-             << " to point at " << dial << "." << endl;
+             << " to point at " << dial
+             << "; during this rotation, it points at 0 " << pointAtZero
+             << "-times." << endl;
     }
 
     return count;
 }
+
 
 int main(int argc, char** argv) {
     if (argc != 2) {
