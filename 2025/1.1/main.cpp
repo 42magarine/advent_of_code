b79b173b9622d34c97d7e42648b0a71a72ed7f14
magarine <magarine@mailbox.org>
2025-12-01 15:34:10 +0100

    day1 part1

---
+++ 2025/1.1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    (void) argc;
+    
+    ifstream file(argv[1]);
+    string line;
+    vector<pair<char, int>> input;
+
+    while (getline(file, line)) {
+        pair<char, int> rotation;
+        rotation.first = line[0]; // 'L' or 'R'
+        rotation.second = stoi(line.substr(1)); // number 0 - 99
+        input.push_back(rotation);
+    }
+
+    file.close();
+
+    int count = 0;
+    int start = 50;
+    for (auto rotation : input) {
+        if (rotation.first == 'L') {
+            start = ((start + rotation.second) + 100) % 100;
+        }
+        else if (rotation.first == 'R') {
+            start = ((start - rotation.second) + 100) % 100;
+        }
+        else {
+            cout << "error" << endl;
+        }
+
+        if (start == 0) {
+            count++;
+        }
+    }
+
+    std::cout<< "count: " << count << endl;
+    return 0;
+}
